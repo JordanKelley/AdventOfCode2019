4f4b5962ba13fb6c0b06b86f737646ce94aed0cf
Jordan Kelley <jordan_kelley@choosebroadspire.com>
2019-12-02 16:13:38 -0600

    Day one complete

---
+++ AdventOfCode2019/ConsoleApp1/Days/One/DayOne.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1.Days.One
+{
+    public static class DayOne
+    {
+        public static int PartOne()
+        {
+            List<int> testInput = Input.TestInput;
+
+            int totalFuelRequired = 0;
+
+            foreach(int input in testInput)
+            {
+                int fuelRequired = input / 3 - 2;
+                totalFuelRequired += fuelRequired;
+            }
+            return totalFuelRequired;
+        }
+
+        public static int PartTwo()
+        {
+            List<int> testInput = Input.TestInput;
+
+            int totalFuelRequired = 0;
+
+            foreach(int input in testInput)
+            {
+                int dividedByThree = input / 3 - 2;
+                int remainder = 0;
+                
+                while(dividedByThree >= 0)
+                {
+                    remainder += dividedByThree;
+                    dividedByThree = dividedByThree / 3 - 2;
+                }
+                totalFuelRequired += remainder;
+            }
+            return totalFuelRequired;
+        }
+    }
+}
---
+++ AdventOfCode2019/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ConsoleApp1.Days.One;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DayOne.PartTwo());
+        }
+    }
+}
